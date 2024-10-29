29edef687624fefcb2cfd07717fc37b9183b024e
Sigma <juliaskolska@gmail.com>
2024-10-29 13:46:42 +0100

    ugabuga highscore

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -9,10 +9,16 @@ public class ScoreUI : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject distanceCount;
     [SerializeField] private GameObject counter;
-    [SerializeField] public TextMeshProUGUI urScore;   
-    [SerializeField] public Canvas deathCanvas;       
-    [SerializeField] public Canvas gameCanvas;      
+    [SerializeField] public TextMeshProUGUI urScore;    
+    [SerializeField] public TextMeshProUGUI highScoreText;  
+    [SerializeField] public Canvas deathCanvas;
+    [SerializeField] public Canvas gameCanvas;
     private bool isGameOver = false;
+
+    void Start()
+    {
+        deathCanvas.gameObject.SetActive(false);
+    }
 
     void Update()
     {
@@ -27,11 +33,25 @@ public class ScoreUI : MonoBehaviour
     public void ShowFinalScore()
     {
         isGameOver = true;
-        
+
+
         gameCanvas.gameObject.SetActive(false);
         deathCanvas.gameObject.SetActive(true);
-        
+
+       
         int finalScore = player.GetComponent<PlayerScore>().Score;
         urScore.text = "Your Score: " + finalScore.ToString();
+
+        
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);  
+        if (finalScore > highScore)
+        {
+            highScore = finalScore;
+            PlayerPrefs.SetInt("HighScore", highScore);    
+            PlayerPrefs.Save();
+        }
+
+        
+        highScoreText.text = "High Score: " + highScore.ToString();
     }
 }
